daa419402aeae9b5c0c8e6cac15faa338d4ae1a5
Abdul Rahman Quraishi <abdul.quraishi@10xengineers.ai>
2023-03-14 18:14:51 +0500

    Part 2 Completed

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -21,5 +21,24 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    int len = strlen(dna_seq->sequence);
+    dna_seq->A_count = 0;
+    dna_seq->C_count = 0;
+    dna_seq->G_count = 0;
+    dna_seq->T_count = 0;
+    for(int i = 0; i < len; i++) {
+	if(dna_seq->sequence[i] == 'A') {
+	    dna_seq->A_count++;
+	}
+	if(dna_seq->sequence[i] == 'C') {
+	    dna_seq->C_count++;
+	}	
+	if(dna_seq->sequence[i] == 'G') {
+	    dna_seq->G_count++;
+	}	
+	if(dna_seq->sequence[i] == 'T') {
+	    dna_seq->T_count++;
+	}
+    }
     return;
 }
--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -40,6 +40,14 @@ int main(int argc, char **argv) {
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
 
+    DNA_sequence dna_seq_3;
+    strcpy(dna_seq_3.sequence, "ABCDEFGFEDCBA");
+    compute_nucleotide_occurrences(&dna_seq_3);
+    assert(dna_seq_3.A_count == 2);
+    assert(dna_seq_3.C_count == 2);
+    assert(dna_seq_3.G_count == 1);
+    assert(dna_seq_3.T_count == 0);
+
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
     return 0;
