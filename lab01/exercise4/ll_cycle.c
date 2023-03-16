d71a37813b570b7dabda6f7cf80f2efe13a7d605
Abdul Rahman Quraishi <abdul.quraishi@10xengineers.ai>
2023-03-16 11:56:59 +0500

    Exercise 4 Completed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,25 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) {
+	return 0;
+    }
+
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+
+    while (1) {
+        fast_ptr = fast_ptr->next;
+	if (fast_ptr == NULL) {
+ 	    return 0;
+	}
+	fast_ptr = fast_ptr->next;
+	if (fast_ptr == NULL) {
+	    return 0;
+	}
+	slow_ptr = slow_ptr->next;
+	if (fast_ptr == slow_ptr) {
+	    return 1;
+	} 
+    }
 }
